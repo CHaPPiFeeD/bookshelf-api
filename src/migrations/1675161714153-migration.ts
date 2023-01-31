4c9f417a180150bd8b41ac7bbd95aa38fa4ea401
Dmitry Shevchuk <77878363+CHaPPiFeeD@users.noreply.github.com>
2023-01-31 16:09:34 +0200

    fixed issue of migrations

--- src/migrations/1675161714153-migration.ts
+++ src/migrations/1675161714153-migration.ts
@@ -1,23 +1,24 @@
+/* eslint-disable quotes */
 /* eslint-disable @typescript-eslint/no-empty-function */
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class migration1675161714153 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`INSERT INTO users 
+    await queryRunner.query(`INSERT INTO public.user
       (nickname, password)
       VALUES ('Chapka', '123456')
     `);
 
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Комедія")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Романтика")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Фентезі")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Детектив")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Боєвик")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Містика")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Пригоди")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Жахи")');
-    await queryRunner.query('INSERT INTO genres (name) VALUES ("Фантастика")');
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Комедія')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Романтика')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Фентезі')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Детектив')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Боєвик')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Містика')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Пригоди')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Жахи')`);
+    await queryRunner.query(`INSERT INTO genre (name) VALUES ('Фантастика')`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
